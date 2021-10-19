124274036aa8cc444a90789321993f29cdb601ad
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-19 22:37:18 +0530

    add the search feature

--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def home(req):
-    import requests
-    import json
-    
     # price requests
     price_req = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,USDT,LTC,EOS,BNB,ADA&tsyms=USD")
     price = json.loads(price_req.content)
@@ -19,6 +18,8 @@ def home(req):
 def prices(req):
     if req.method == 'POST':
         quote = req.POST['quote']
-        return render(req, 'price.html', {'quote': quote})
+        quote_req = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+quote+'&tsyms=USD')
+        price = json.loads(quote_req.content)
+        return render(req, 'price.html', {'quote': quote, 'price': price})
     else:
         return render(req, "price.html", {})
